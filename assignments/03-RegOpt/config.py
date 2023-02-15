ea12ddcbb95e274a85a727946a3e1999c4920fce
Mariana Luna <67344188+marilunazz@users.noreply.github.com>
2023-02-15 15:01:19 -0500

    cosine scheduler initial trial

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,19 +8,22 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 20
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "num_epochs": num_epochs
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,14 @@
 from typing import List
-
+import math
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Learning rate scheduler based on exponential decay.
+    """
+
+    def __init__(self, optimizer, num_epochs, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -13,12 +17,53 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        self.optimizer = optimizer
+        self.last_epoch = last_epoch
+        self.num_epochs = num_epochs
+        self.max_update = 40
+        self.final_lr = 0
+        self.warmup_steps = 0
+        self.warmup_begin_lr = 0
+        self.max_steps = self.max_update - self.warmup_steps
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
-    def get_lr(self) -> List[float]:
+    def get_warmup_lr(self, epoch):
+        increase = (
+            (self.base_lrs[0] - self.warmup_begin_lr)
+            * float(epoch)
+            / float(self.warmup_steps)
+        )
+        return self.warmup_begin_lr + increase
+
+    def get_lr(
+        self,
+    ) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        """
+        Gets the learning rate
+        """
+        # k = 0.1
+        # print(self.base_lrs)
+        # lrs = [self.base_lrs[0] * (-k**epoch) for epoch in range(self.num_epoch + 1)]
+        self.base_lr_orig = self.base_lrs[0]
+        lrs = []
+        for epoch in range(self.num_epochs):
+            if epoch < self.warmup_steps:
+                lrs.append(self.get_warmup_lr(epoch))
+            if epoch <= self.max_update:
+                lrs.append(
+                    self.final_lr
+                    + (self.base_lr_orig - self.final_lr)
+                    * (
+                        1
+                        + math.cos(
+                            math.pi * (epoch - self.warmup_steps) / self.max_steps
+                        )
+                    )
+                    / 2
+                )
+        return lrs
