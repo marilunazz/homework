9e50804e22fa27a80c0a7423b5f74c9c5dd13c9d
Mariana Luna <67344188+marilunazz@users.noreply.github.com>
2023-02-16 17:24:05 -0500

    final changes

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -20,50 +20,23 @@ class CustomLRScheduler(_LRScheduler):
         self.optimizer = optimizer
         self.last_epoch = last_epoch
         self.num_epochs = num_epochs
-        self.max_update = 40
+        self.max_steps = 40
         self.final_lr = 0
-        self.warmup_steps = 0
-        self.warmup_begin_lr = 0
-        self.max_steps = self.max_update - self.warmup_steps
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
-
-    def get_warmup_lr(self, epoch):
-        increase = (
-            (self.base_lrs[0] - self.warmup_begin_lr)
-            * float(epoch)
-            / float(self.warmup_steps)
-        )
-        return self.warmup_begin_lr + increase
 
     def get_lr(
         self,
     ) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
-
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
         """
         Gets the learning rate
         """
-        # k = 0.1
-        # print(self.base_lrs)
-        # lrs = [self.base_lrs[0] * (-k**epoch) for epoch in range(self.num_epoch + 1)]
-        self.base_lr_orig = self.base_lrs[0]
         lrs = []
         for epoch in range(self.num_epochs):
-            if epoch < self.warmup_steps:
-                lrs.append(self.get_warmup_lr(epoch))
-            if epoch <= self.max_update:
+            if epoch <= self.max_steps:
                 lrs.append(
                     self.final_lr
-                    + (self.base_lr_orig - self.final_lr)
-                    * (
-                        1
-                        + math.cos(
-                            math.pi * (epoch - self.warmup_steps) / self.max_steps
-                        )
-                    )
+                    + (self.base_lrs[0] - self.final_lr)
+                    * (1 + math.cos(math.pi * epoch / self.max_steps))
                     / 2
                 )
         return lrs
