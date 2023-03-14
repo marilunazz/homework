e8b14fa7e675491049eae39d8ab4eb17c26d2ff9
Mariana Luna <67344188+marilunazz@users.noreply.github.com>
2023-03-14 18:28:28 -0400

    pistachio

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -15,10 +15,9 @@ class Model(torch.nn.Module):
 
         self.conv1 = nn.Conv2d(num_channels, 12, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(2352, 120)  # used to be 16*5*5, 120
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(120, self.num_classes)
+        self.fc2 = nn.Linear(120, self.num_classes)
+        self.drop = nn.Dropout(0.15)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -33,14 +32,11 @@ class Model(torch.nn.Module):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.pool(x)
-
-        # x = self.conv2(x)
-        # x = F.relu(x)
-        # x = self.pool(x)
+        x = self.drop(x)
 
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
 
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
